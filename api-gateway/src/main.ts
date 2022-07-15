d37236a6b5449d457e45e0557552cb418799d246
Gabriel Mazurco <mazurco066@gmail.com>
2022-07-15 15:55:53 -0300

    feat(dependencies): added starter dependencies to api-gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,38 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// Dependencies
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import * as momentTimezone from 'moment-timezone'
+
+// Server implementation dependencies
+import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './filters'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  // DTO Configuration
+  const validationOptions = {
+    skipMissingProperties: false,
+    validationError: { target: false },
+    validateCustomDecorators: true
+  }
+
+  // Pipes, filters and prefixes
+  app.useGlobalPipes(new ValidationPipe(validationOptions))
+  app.useGlobalFilters(
+    new HttpExceptionFilter()
+  )
+
+  // Date default format
+  Date.prototype.toJSON = function() : any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
+
+  // Assign server port
+  await app.listen(3001)
 }
-bootstrap();
+
+// Starting api-gateway
+bootstrap()
