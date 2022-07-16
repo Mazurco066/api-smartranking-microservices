9aa7f417d082935c8dbc68449b2a786548fdccdd
Gabriel Mazurco <mazurco066@gmail.com>
2022-07-15 22:07:24 -0300

    feat(api-gateway): added category interceptors
    ]

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,6 +1,38 @@
-import { Controller } from '@nestjs/common';
+// Dependencies
+import { Body, Controller, Post, Put, Logger, Param } from '@nestjs/common'
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices'
 
-@Controller()
+// Implementations
+import { AddCategoryDTO, UpdateCategoryDTO } from './dtos'
+import { ValidateParamsPipe } from './pipes'
+
+@Controller('api/v1')
 export class AppController {
-  constructor() {}
+  // Class properties
+  private logger = new Logger(AppController.name)
+  private clientAdminBackend: ClientProxy
+
+  // Class constructor
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://guest:guest@localhost:5672/smartranking'],
+        queue: 'admin-backend'
+      }
+    })
+  }
+
+  @Post('categories')
+  addCategory(@Body() body: AddCategoryDTO) {
+    return this.clientAdminBackend.emit('add-category', body)
+  }
+  
+  @Put('categories/:id')
+  async updateCategory(
+    @Param('id', ValidateParamsPipe) id: string,
+    @Body() body: UpdateCategoryDTO
+  ) {
+    return this.clientAdminBackend.emit('update-category', body)
+  }
 }
--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// Dependencies
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ AppController ],
+  providers: []
 })
 export class AppModule {}
